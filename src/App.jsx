9acc1244ee465f061541e98dcb08776ff5c27025
Avipsa <avipsapradhan2003@gmail.com>
2023-12-14 01:32:49 +0530

    Avipsa commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import SoilAnalysis from './pages/SoilAnalysis'
 import Home from './pages/Home'
+import CropCure from './pages/CropCure'
 import { Route, Routes } from "react-router-dom"
 
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/soilanalysis" element={<SoilAnalysis/>}/>
+        <Route path="/cropcure" element={<CropCure/>}/>
       </Routes>
       </div>
 
@@ -19,4 +21,4 @@ function App() {
   )
 }
 
-export default App
+export default App
--- src/components/HeadingSoil.jsx
+++ src/components/HeadingSoil.jsx
@@ -1,13 +1,12 @@
-
-
-const Heading = () => {
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { Link } from 'react-router-dom';
+const HeadingSoil = () => {
     return (
-      <div className="h-200  bg-white flex justify-center items-center border-t-2 border-b-2 border-green-800">
-      <div className="p-4">
-          <p className="text-2xl text-green-800 font-orelega font-extrabold">Soil Analysis</p>
+      <div className="h-200  bg-white flex justify-center items-center border-t-2 border-b-2 border-green-800 p-2">
+        <Link to="/" ><IoIosArrowDropleftCircle className="fill-green-900 w-10 h-10 mr-16"/></Link>
+        <div className="text-2xl text-green-800 font-orelega font-extrabold mr-28">Soil Analysis</div>
       </div>
-  </div>
     )
   }
   
-  export default Heading
+  export default HeadingSoil
