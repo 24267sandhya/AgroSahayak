b9a663ca97092b73c9fcf3308561c39b1b579a1a
kknowlege <kamalsaini2673@gmail.com>
2023-12-14 21:59:41 +0530

    my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import SoilAnalysis from './pages/SoilAnalysis'
 import Home from './pages/Home'
-<<<<<<< HEAD
 import CropPlan from './pages/CropPlan'
 import NextTask1 from './pages/NextTask1'
 import NextTask2 from './pages/NextTask2'
@@ -12,9 +11,6 @@ import NextTask6 from './pages/NextTask6'
 import NextTask7 from './pages/NextTask7'
 import Community from './pages/Community'
 import Mypost from './pages/Mypost'
-=======
-import CropCure from './pages/CropCure'
->>>>>>> 0073ac84ce4d66e5b0bb6be2be1e24d661de3848
 import { Route, Routes } from "react-router-dom"
 import CropCureProductDetail from './pages/CropCureProductDetail'
 
@@ -27,7 +23,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/soilanalysis" element={<SoilAnalysis/>}/>
-<<<<<<< HEAD
         <Route path="/CropPlan" element={<CropPlan/>}/>
         <Route path="/NextTask1" element={<NextTask1/>}/>
         <Route path="/NextTask2" element={<NextTask2/>} />
@@ -38,10 +33,8 @@ function App() {
         <Route path="/NextTask7" element={<NextTask7 />} />
         <Route path="/Community" element={<Community/>} />
         <Route path="/Mypost" element={<Mypost/>}/>
-=======
-        <Route exact path="/cropcureproductdetail" element={<CropCureProductDetail/>}/>
-        <Route exact path="/cropcure" element={<CropCure/>}/>
->>>>>>> 0073ac84ce4d66e5b0bb6be2be1e24d661de3848
+        <Route path="/CropCureProductDetail" element={<CropCureProductDetail />} />
+
       </Routes>
       </div>
 
